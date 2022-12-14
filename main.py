214f9b40898a2bfc85b5fb0db1a9d1b322a5ec7b
mari <altysheva_maria@mail.ru>
2022-12-14 14:05:49 +0300

    темка

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import base64
 import io
 from io import StringIO
 
@@ -34,3 +35,19 @@ def load_image():
 img = load_image()
 
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+add_bg_from_local('for_guys.png')
